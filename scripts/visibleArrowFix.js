62e050ef8663e0c1a1da22dc5d9b1582b9420b0a
Michał Myszor <45109417+MItCHeLPL@users.noreply.github.com>
2020-09-19 15:39:17 +0200

    impoved css, aded blur and shadows

--- scripts/visibleArrowFix.js
+++ scripts/visibleArrowFix.js
@@ -1,8 +1,30 @@
+//Dynamic
 //Add margin to section if section is overlapping with scroll arrow
 $(window).on('load resize', function(){
     $("section").each(function(){
+
+        let div = $(this).outerHeight();
+        let win = $(window).height();
+
+        if((div/win) > 0.6)
+        {
+            //normalized = newMin + (val - minVal) * (newMax - newMin) / (maxVal - minVal);
+            let margin = 0 + ((div/win) - 0) * (7.5 - 0) / (1 - 0);
+    
+            if ($(this).hasClass("disable-arrowFix") == false) 
+            {
+                $(this).css('margin-bottom', margin+"em");
+            }
+        } 
+    });
+});
+
+//Forced class
+//Add margin to section if section is overlapping with scroll arrow
+/*$(window).on('load resize', function(){
+    $("section").each(function(){
         var div = $(this).outerHeight();
-        var win = $(window).height()*0.9;
+        var win = $(window).height()*0.75;
     
         if (div > win && $(this).hasClass("disable-arrowFix") == false) 
         {
@@ -13,4 +35,4 @@ $(window).on('load resize', function(){
             $(this).removeClass('arrowFix');
         }
     });
-});
+});*/
